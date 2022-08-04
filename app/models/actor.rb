999cb494e442414d3937c52e5cd26a0e4f92a512
+ <+>
2022-08-03 18:25:31 -0600

    commit tree a9d6edd7ac23b9b0f8f4114d052005ab1abb1284

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,14 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
+
+  def list_roles
+    #self. "#{self.characters.map(&:name)} - #{self.shows.map(&:name)}"
+    self.characters.map{ |roles| "#{roles.name} - #{roles.show.name}"}
+  end
+
 end
